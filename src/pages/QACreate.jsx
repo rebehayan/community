f5d01dd782e42781a0f141e6e3d063bf30842954
rebehayan <rebehayan@gmail.com>
2025-03-28 22:40:05 +0900

    글쓰기 유효성검사 수정

--- src/pages/QACreate.jsx
+++ src/pages/QACreate.jsx
@@ -5,7 +5,13 @@ import Select from "../components/Molecules/Select";
 import Button from "../components/Molecules/Button";
 import Block from "../components/Molecules/Block";
 import Heading from "../components/Molecules/Heading";
+import Portal from "../util/Portal";
+import Toast from "../components/Molecules/Toast";
 const selectCategory = [
+  {
+    value: "",
+    title: "선택하세요",
+  },
   {
     value: "catetory1",
     title: "선택1",
@@ -25,6 +31,9 @@ export default function QACreate() {
     category: "",
     description: "",
   });
+  const [error, setError] = useState("");
+  const [toast, setToast] = useState(false);
+
   const handleForm = (name, value) => {
     setFormData((prev) => {
       return {
@@ -35,26 +44,52 @@ export default function QACreate() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!formData.category.trim()) {
+      setToast(true);
+      setError("카테고리를 선택해주세요.");
+      return;
+    }
+    if (!formData.description.trim()) {
+      setToast(true);
+      setError("내용을 넣어주세요.");
+      return;
+    }
+    if (!formData.title.trim()) {
+      setToast(true);
+      setError("제목을 넣어주세요.");
+      return;
+    }
+
+    setFormData({
+      title: "",
+      category: "",
+      description: "",
+    });
     console.log(formData);
   };
   return (
-    <Block>
-      <Heading text="Post a Question" level="2" />
-      <div style={{ "--col": "20rem" }}>
-        <form onSubmit={handleSubmit}>
-          <Input type="text" placeholder="제목을 입력하세요." name="title" label="제목" error={false} onChange={handleForm} />
-          <Select label="카테고리" options={selectCategory} name="category" onChange={handleForm} />
-          <BoardWrite name="description" onChange={handleForm} />
-          <div className="align both">
-            <Button color="white-outline" size="base" type="submit">
-              목록
-            </Button>
-            <Button color="primary" size="base" type="submit">
-              글쓰기
-            </Button>
-          </div>
-        </form>
-      </div>
-    </Block>
+    <>
+      <Block>
+        <Heading text="Post a Question" level="2" />
+        <div style={{ "--col": "20rem" }}>
+          <form onSubmit={handleSubmit}>
+            <Input type="text" placeholder="제목을 입력하세요." name="title" label="제목" value={formData.title} error={false} onChange={handleForm} />
+            <Select label="카테고리" options={selectCategory} name="category" onChange={handleForm} value={formData.category} />
+            <BoardWrite name="description" onChange={handleForm} />
+            <div className="mt30 align both">
+              <Button color="white-outline" size="base" type="submit">
+                목록
+              </Button>
+              <Button color="primary" size="base" type="submit">
+                글쓰기
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Block>
+      <Portal selector="#toast">
+        <Toast state={toast} message={error} onClose={() => setToast(false)} />
+      </Portal>
+    </>
   );
 }
